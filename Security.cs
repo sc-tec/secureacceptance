52d06bfb344b0e5c6279ad8413c2bf067e9e0220
Steven Chong <steven_chong@executivecentre.com>
2020-04-07 15:00:21 +0800

    first commit
    Signed-off-by: Steven Chong <steven_chong@executivecentre.com>

---
+++ Security.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections.Specialized;
+
+namespace secureacceptance
+{
+    public static class Security
+    {
+        private const String SECRET_KEY = "<REPLACE WITH SECURITY KEY>";
+
+        public static String sign(IDictionary<string, string> paramsArray)  {
+            return sign(buildDataToSign(paramsArray), SECRET_KEY);
+        }
+
+        private static String sign(String data, String secretKey) {
+            UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            byte[] keyByte = encoding.GetBytes(secretKey);
+
+            HMACSHA256 hmacsha256 = new HMACSHA256(keyByte);
+            byte[] messageBytes = encoding.GetBytes(data);
+            return Convert.ToBase64String(hmacsha256.ComputeHash(messageBytes));
+        }
+
+        private static String buildDataToSign(IDictionary<string,string> paramsArray) {
+            String[] signedFieldNames = paramsArray["signed_field_names"].Split(',');
+            IList<string> dataToSign = new List<string>();
+
+	        foreach (String signedFieldName in signedFieldNames)
+	        {
+	             dataToSign.Add(signedFieldName + "=" + paramsArray[signedFieldName]);
+	        }
+
+            return commaSeparate(dataToSign);
+        }
+
+        private static String commaSeparate(IList<string> dataToSign) {
+            return String.Join(",", dataToSign);                         
+        }
+    }
+}
---
+++ payment_confirmation.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="payment_confirmation.aspx.cs" Inherits="secureacceptance.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+    <title>Secure Acceptance - Payment Form Example</title>
+    <link rel="stylesheet" type="text/css" href="Styles/payment.css"/>
+</head>
+<body>
+<form id="payment_confirmation" action="https://testsecureacceptance.cybersource.com/pay" method="post"/>
+<fieldset id="confirmation">
+    <legend>Review Payment Details</legend>
+    <div>
+        <%
+            foreach (var key in Request.Form.AllKeys)
+            { 
+                Response.Write("<div>");
+                Response.Write("<span class=\"fieldName\">" + key + ":</span><span class=\"fieldValue\">" + Request.Params[key] + "</span>");
+                Response.Write("</div>");
+            }
+        %>
+    </div>
+</fieldset>
+    <%
+        IDictionary<string, string> parameters = new Dictionary<string, string>();
+        foreach (var key in Request.Form.AllKeys)
+        {
+            Response.Write("<input type=\"hidden\" id=\"" + key + "\" name=\"" + key + "\" value=\"" + Request.Params[key] + "\"/>\n");
+            parameters.Add(key, Request.Params[key]);
+        }
+        Response.Write("<input type=\"hidden\" id=\"signature\" name=\"signature\" value=\"" + secureacceptance.Security.sign(parameters) + "\"/>\n");
+    %>
+<input type="submit" id="submit" value="Confirm"/>
+</form>
+</body>
+</html>
